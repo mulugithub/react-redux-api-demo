a7bc0f42f8a616c357a68b6df9caf6a6c187f045
Muluken Zemed <zmmulu@gmail.com>
2023-09-07 21:48:54 +0300

    Fix eslint errors on fetchedUsers component

--- src/components/fetchedUsers.jsx
+++ src/components/fetchedUsers.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchUsers } from "../users/usersSlice";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from '../users/usersSlice';
 
 const Users = () => {
   const { users, isLoading, error } = useSelector((state) => state.users);
@@ -13,16 +11,21 @@ const Users = () => {
   }, [dispatch]);
 
   return (
-    <><h1>Users List</h1>
-    <ul>
-      {isLoading && <li>Loading...</li>}
-      {error && <li>{error}</li>}
-      {users.map((user) => (
-        <li key={user.email}>{user.name.first} {user.name.last}</li>
-      ))}
-    </ul>
+    <>
+      <h1>Users List</h1>
+      <ul>
+        {isLoading && <li>Loading...</li>}
+        {error && <li>{error}</li>}
+        {users.map((user) => (
+          <li key={user.email}>
+            {user.name.first}
+            {' '}
+            {user.name.last}
+          </li>
+        ))}
+      </ul>
     </>
   );
-}
+};
 
 export default Users;
